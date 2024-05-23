9bbd8a77596a2ffc5d7be6b098a42b548acc48ed
ArtAulas <carelliarthur@gmail.com>
2024-05-23 11:09:51 -0300

    2305

--- routes_fastapi.py
+++ routes_fastapi.py
@@ -3,10 +3,10 @@ from fastapi import FastAPI
 
 app=FastAPI()
 
-@app.get("/health-check")
-def get_health():
-    return {'status':'Saudável'}
+@app.get("/helloworld")
+def helloworld():
+    return 'Hello World'
 
 if __name__=='__main__':
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8003, reload=True)
+    uvicorn.run("routes_fastapi:app", host="127.0.0.1", port=8003, reload=True)
